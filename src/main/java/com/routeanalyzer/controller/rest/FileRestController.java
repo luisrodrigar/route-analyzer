56cae2189b9930881c3c6f52820c06bcd29b7d33
Luis Ángel Rodriguez Garcia <luis.rodriguez@mitulagroup.com>
2018-04-06 11:16:00 +0200

    Fixed some warnings

--- src/main/java/com/routeanalyzer/controller/rest/FileRestController.java
+++ src/main/java/com/routeanalyzer/controller/rest/FileRestController.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBException;
 
 import org.springframework.beans.factory.annotation.Autowired;
