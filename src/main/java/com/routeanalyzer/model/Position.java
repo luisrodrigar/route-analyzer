e3de8b0c7ccfbc4c0aa00b6a4b5eaf865efabd04
Luis Angel Rodriguez Garcia <luisrodrigar11@gmail.com>
2019-02-12 20:37:50 +0100

    Define the maven compiler version to avoid problems with older maven compiler plugins and lombok.

--- src/main/java/com/routeanalyzer/model/Position.java
+++ src/main/java/com/routeanalyzer/model/Position.java
@@ -17,35 +17,4 @@ public class Position implements Serializable {
 	private BigDecimal latitudeDegrees;
 	private BigDecimal longitudeDegrees;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((latitudeDegrees == null) ? 0 : latitudeDegrees.hashCode());
-		result = prime * result + ((longitudeDegrees == null) ? 0 : longitudeDegrees.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Position other = (Position) obj;
-		if (latitudeDegrees == null) {
-			if (other.latitudeDegrees != null)
-				return false;
-		} else if (!latitudeDegrees.equals(other.latitudeDegrees))
-			return false;
-		if (longitudeDegrees == null) {
-			if (other.longitudeDegrees != null)
-				return false;
-		} else if (!longitudeDegrees.equals(other.longitudeDegrees))
-			return false;
-		return true;
-	}
-
 }
