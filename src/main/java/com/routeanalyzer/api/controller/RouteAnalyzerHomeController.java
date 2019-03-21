961803f473ff975b7728a12c1cea9ffa26d19a1a
Luis Angel Rodriguez <rodriguezla@ryanair.com>
2019-03-21 11:55:53 +0100

    Added template thymeleeaf processor to generate the home page in order to define its endpoints.

--- src/main/java/com/routeanalyzer/api/controller/RouteAnalyzerHomeController.java
+++ src/main/java/com/routeanalyzer/api/controller/RouteAnalyzerHomeController.java
@@ -1,28 +1,23 @@
 
 package com.routeanalyzer.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @RestController
 public class RouteAnalyzerHomeController {
+
+	@Autowired
+	private TemplateEngine templateGenerator;
+
+	private static final String HOME_PAGE = "home";
+
 	@RequestMapping(value = "/")
 	public String helloWorld() {
-		String methods = "<ul><em>File</em></br></br><li style='text-align:center;list-style:none;'>"
-				+ "<ul><strong>[POST]/upload</strong> : uploading a route (xml: tcx/gpx)</ul>"
-				+ "<ul><strong>[GET]/get/{type}/{id}</strong> : get a file from AWS S3</ul>" + "</li></ul>"
-				+ "<ul><em>Activity</em></br></br><li style='text-align:center;list-style:none;'>"
-				+ "<ul><strong>[GET]/{id}</strong> : get an activity</ul>"
-				+ "<ul><strong>[GET]/{id}/export/{type}</strong> : export the activity with current modifications</ul>"
-				+ "<ul><strong>[PUT]/{id}/remove/point</strong> : remove an activity's point</ul>"
-				+ "<ul><strong>[PUT]/{id}/join/laps</strong> : join two continuous activity's laps </ul>"
-				+ "<ul><strong>[PUT]/{id}/split/lap</strong> : split one lap into two by the given point </ul>"
-				+ "<ul><strong>[PUT]/{id}/remove/laps</strong> : remove point </ul>"
-				+ "<ul><strong>[PUT]/{id}/color/laps</strong> : set color laps </ul>" + "</li></ul>";
-		String title = "<br><div style='text-align:center;'>"
-				+ "<h1>Route Analyzer</h1><hr><p>API for Route Analyzer web app which provides several methods to manipulate data from an activity route.</p><p>You can access to the website in this link: <a href='https://routeanalyzer.herokuapp.com/'>Route Analyzer Web</a></p><p>The different methods of the interface implemented:</p><li style='text-align:center;list-style:none;'>"
-				+ methods + "</li></div>";
-
-		return title;
+		Context context = new Context();
+		return templateGenerator.process(HOME_PAGE, context);
 	}
 }
