fd649912cb909b44d9d24fe29095c14394064880
Luis Angel Rodriguez <rodriguezla@ryanair.com>
2019-05-20 00:51:16 +0200

    Added restTemplate as a bean of Spring. Using lombok for google maps classes. Refactor code with java 8 functional syntax and creating some test cases.

--- src/main/java/com/routeanalyzer/api/services/googlemaps/GMPosition.java
+++ src/main/java/com/routeanalyzer/api/services/googlemaps/GMPosition.java
@@ -1,26 +1,18 @@
 package com.routeanalyzer.api.services.googlemaps;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GMPosition {
 	private Double lat;
 	private Double lng;
-	public Double getLat() {
-		return lat;
-	}
-	public void setLat(Double lat) {
-		this.lat = lat;
-	}
-	public Double getLng() {
-		return lng;
-	}
-	public void setLng(Double lng) {
-		this.lng = lng;
-	}
-	@Override
-	public String toString() {
-		return "GMPosition [lat=" + lat + ", lng=" + lng + "]";
-	}
-	
 }
