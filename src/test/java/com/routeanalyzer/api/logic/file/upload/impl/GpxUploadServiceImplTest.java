fda4d502c5d4289268b4b39bf345bab5d21fef96
Rodriguez <rodriguezla@rl3320011405.local>
2020-02-18 23:10:44 +0100

    added some test

--- src/test/java/com/routeanalyzer/api/logic/file/upload/impl/GpxUploadServiceImplTest.java
+++ src/test/java/com/routeanalyzer/api/logic/file/upload/impl/GpxUploadServiceImplTest.java
@@ -1,19 +1,24 @@
 package com.routeanalyzer.api.logic.file.upload.impl;
 
-import com.routeanalyzer.api.common.JsonUtils;
 import com.routeanalyzer.api.logic.ActivityOperations;
 import com.routeanalyzer.api.logic.LapsOperations;
 import com.routeanalyzer.api.model.Activity;
 import com.routeanalyzer.api.services.reader.GPXService;
+import com.routeanalyzer.api.xml.gpx11.ExtensionsType;
 import com.routeanalyzer.api.xml.gpx11.GpxType;
+import com.routeanalyzer.api.xml.gpx11.MetadataType;
+import com.routeanalyzer.api.xml.gpx11.TrkType;
+import com.routeanalyzer.api.xml.gpx11.TrksegType;
+import com.routeanalyzer.api.xml.gpx11.WptType;
+import com.routeanalyzer.api.xml.gpx11.trackpointextension.garmin.TrackPointExtensionT;
 import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
@@ -23,21 +28,29 @@ import org.xml.sax.SAXParseException;
 import utils.TestUtils;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.GregorianCalendar;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static java.util.Collections.emptyList;
+import static com.routeanalyzer.api.common.JsonUtils.fromJson;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static utils.TestUtils.getFileBytes;
 
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 public class GpxUploadServiceImplTest {
 
-    @Spy
+    @Mock
     private GPXService gpxService;
 
     @Mock
@@ -60,18 +73,105 @@ public class GpxUploadServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        gpxObject = gpxService.readXML(gpxXmlResource.getInputStream()).get();
-        String jsonActivityGpxStr = new String(TestUtils.getFileBytes(activityGpxResource), StandardCharsets.UTF_8);
-        activityGpxTest = JsonUtils.fromJson(jsonActivityGpxStr, Activity.class)
-                .getOrNull();
+        gpxObject = createValidGpxType();
+        String jsonActivityGpxStr = new String(getFileBytes(activityGpxResource), UTF_8);
+        activityGpxTest = fromJson(jsonActivityGpxStr, Activity.class).getOrNull();
+    }
+
+    private GpxType createValidGpxType() {
+        GpxType gpxType = new GpxType();
+        gpxType.setMetadata(createMetadata());
+        gpxType.setCreator("Garmin Connect");
+        gpxType.addTrk(createTrkType());
+        return gpxType;
+    }
+
+    private MetadataType createMetadata() {
+        MetadataType metadataType = new MetadataType();
+        LocalDateTime localDateTime = LocalDateTime.of(2020, 2, 13, 11, 41);
+        metadataType.setTime(getXmlGregorianCalendar(ZonedDateTime.of(localDateTime, ZoneId.systemDefault())));
+        return metadataType;
+    }
+
+    private TrkType createTrkType() {
+        TrkType trkType = new TrkType();
+        trkType.addTrkseg(createTrksegType1());
+        trkType.addTrkseg(createTrksegType2());
+        trkType.addTrkseg(createTrksegType3());
+        return trkType;
+    }
+
+    private TrksegType createTrksegType1() {
+        TrksegType trksegType = new TrksegType();
+        LocalDateTime time1 = LocalDateTime.of(2018, 02, 27, 14, 16, 13);
+        LocalDateTime time2 = LocalDateTime.of(2018, 02, 27, 14, 16, 18);
+        LocalDateTime time3 = LocalDateTime.of(2018, 02, 27, 14, 16, 20);
+        trksegType.addTrkpt(createTrkpt(new BigDecimal("42.6131970"), new BigDecimal("-6.5732170"), new BigDecimal(
+                "557.3"), new Short("96"), getXmlGregorianCalendar(ZonedDateTime.of(time1, ZoneId.systemDefault()))));
+        trksegType.addTrkpt(createTrkpt(new BigDecimal("42.6132170"), new BigDecimal("-6.5733730"), new BigDecimal(
+                "557.3"), new Short("96"), getXmlGregorianCalendar(ZonedDateTime.of(time2, ZoneId.systemDefault()))));
+        trksegType.addTrkpt(createTrkpt(new BigDecimal("42.6132120"), new BigDecimal("-6.5734430"), new BigDecimal(
+                "557.3"), new Short("96"), getXmlGregorianCalendar(ZonedDateTime.of(time3, ZoneId.systemDefault()))));
+        return trksegType;
+    }
+
+    private TrksegType createTrksegType2() {
+        TrksegType trksegType = new TrksegType();
+        LocalDateTime time1 = LocalDateTime.of(2018, 02, 27, 14, 16, 30);
+        LocalDateTime time2 = LocalDateTime.of(2018, 02, 27, 14, 16, 33);
+        trksegType.addTrkpt(createTrkpt(new BigDecimal("42.6132120"), new BigDecimal("-6.5738250"), new BigDecimal(
+                "557.3"), new Short("106"), getXmlGregorianCalendar(ZonedDateTime.of(time1, ZoneId.systemDefault()))));
+        trksegType.addTrkpt(createTrkpt(new BigDecimal("42.6132120"), new BigDecimal("-6.5739120"), new BigDecimal(
+                "557.3"), new Short("109"), getXmlGregorianCalendar(ZonedDateTime.of(time2, ZoneId.systemDefault()))));
+        return trksegType;
+    }
+
+    private TrksegType createTrksegType3() {
+        TrksegType trksegType = new TrksegType();
+        LocalDateTime time1 = LocalDateTime.of(2018, 02, 27, 14, 17, 30);
+        trksegType.addTrkpt(createTrkpt(new BigDecimal("42.6132120"), new BigDecimal("-6.5738250"), new BigDecimal(
+                "557.3"), new Short("120"), getXmlGregorianCalendar(ZonedDateTime.of(time1, ZoneId.systemDefault()))));
+        return trksegType;
+    }
+
+    private WptType createTrkpt(final BigDecimal lat, final BigDecimal lon, final BigDecimal elevation,
+                                final Short heartRate, final XMLGregorianCalendar time) {
+        WptType trkpt = new WptType();
+        trkpt.setTime(time);
+        trkpt.setLat(lat);
+        trkpt.setLon(lon);
+        trkpt.setEle(elevation);
+        trkpt.setExtensions(createExtensionType(heartRate));
+        return trkpt;
+    }
+
+    private ExtensionsType createExtensionType(final Short heartRate) {
+        ExtensionsType extensionsType = new ExtensionsType();
+        extensionsType.addAny(createTrackPointExtensionT(heartRate));
+        return extensionsType;
+    }
+
+    private TrackPointExtensionT createTrackPointExtensionT(final Short heartRate) {
+        TrackPointExtensionT trackPointExtensionT = new TrackPointExtensionT();
+        trackPointExtensionT.setHr(heartRate);
+        return trackPointExtensionT;
+    }
+
+    private XMLGregorianCalendar getXmlGregorianCalendar(final ZonedDateTime zonedDateTime) {
+        GregorianCalendar gregorianCalendar = GregorianCalendar.from(zonedDateTime);
+        return Try.of(() -> DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar))
+                        .onFailure(err -> log.error("Error trying to create xml gregorian calendar"))
+                        .getOrNull();
     }
 
     @Test
     public void uploadGpxTest() throws IOException {
         // Given
         MultipartFile multipart = new MockMultipartFile("file", gpxXmlResource.getInputStream());
-        // When
+        doReturn(Try.success(TestUtils.toGpxRootModel(gpxXmlResource)))
+                .when(gpxService).readXML(eq(gpxXmlResource.getInputStream()));
         doReturn(Try.success(gpxObject)).when(gpxService).readXML(Mockito.any());
+        // When
         GpxType result = gpxUploadService.upload(multipart).get();
         // Then
         assertThat(result).isEqualTo(gpxObject);
